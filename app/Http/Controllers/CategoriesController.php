99d311982c491958506e62779d0f9db61f146c1d
nagi <594708461@qq.com>
2018-10-09 13:23:46 +0000

    topic order

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -8,9 +8,14 @@ use Illuminate\Http\Request;
 
 class CategoriesController extends Controller
 {
-    public function show(Category $category)
+    public function show(Category $category, Request $request, Topic $topic)
     {
-       $topics = Topic::with('category','user')->where('category_id',$category->id)->paginate(20);
-       return view('topics.index',compact('topics','category'));
+        // 读取分类 ID 关联的话题，并按每 20 条分页
+        $topics = $topic->withOrder($request->order)
+            ->where('category_id', $category->id)
+            ->paginate(20);
+
+        // 传参变量话题和分类到模板中
+        return view('topics.index', compact('topics', 'category'));
     }
 }
